504297017c5ab06b4e2231ff897bae24f9bd7121
kihwankim <clack2933@gmail.com>
2019-09-17 21:37:36 +0900

    add service class and interface

---
+++ spring-01-web-customer-tracker/src/com/kkh/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.kkh.springdemo.service;
+
+import com.kkh.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+}
---
+++ spring-01-web-customer-tracker/src/com/kkh/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kkh.springdemo.service;
+
+import com.kkh.springdemo.dao.CustomerDAO;
+import com.kkh.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    // need to inject customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Override
+    @Transactional // customerDAO로부터 권한을 위임 받는 곳
+    public List<Customer> getCustomers() {
+        return this.customerDAO.getCustomers();
+    }
+}
