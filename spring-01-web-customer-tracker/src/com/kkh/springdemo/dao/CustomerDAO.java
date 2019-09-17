d69d87dda7a21dbcd6780aaf0f61036145cb5b82
kihwankim <clack2933@gmail.com>
2019-09-17 16:44:17 +0900

    add db dao and entity

---
+++ spring-01-web-customer-tracker/src/com/kkh/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.kkh.springdemo.dao;
+
+import com.kkh.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+}
