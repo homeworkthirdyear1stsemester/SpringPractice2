6ea8ae21d665d78de0ffe6c13863bd15e967df19
kihwankim <clack2933@gmail.com>
2019-09-20 22:59:11 +0900

    wild card it could aspect in method which start anything with "add"

--- spring-02-demo-aop/src/com/kkh/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-02-demo-aop/src/com/kkh/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,7 +14,7 @@ public class MyDemoLoggingAspect {
     // @Before("execution(public void updateAccount())")
 
     // 특정 class의 함수에만 적용
-    @Before("execution(public void com.kkh.aopdemo.dao.AccountDAO.addAccount())")
+    @Before("execution(public void add*())")
     public void beforeAddAccountAdvice() {
         System.out.println("\n============>>> Executing @Before advice on method");
     }
