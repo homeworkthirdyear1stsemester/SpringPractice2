69894460b0cf14a75be79e7afa96881633b501d7
kihwankim <clack2933@gmail.com>
2019-09-20 23:17:35 +0900

    about the params in aspect

---
+++ spring-02-demo-aop/src/com/kkh/aopdemo/Account.java
@@ -0,0 +1,22 @@
+package com.kkh.aopdemo;
+
+public class Account {
+    private String name;
+    private String level;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return this.level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
--- spring-02-demo-aop/src/com/kkh/aopdemo/dao/AccountDAO.java
+++ spring-02-demo-aop/src/com/kkh/aopdemo/dao/AccountDAO.java
@@ -1,11 +1,12 @@
 package com.kkh.aopdemo.dao;
 
+import com.kkh.aopdemo.Account;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccountDAO {
 
-    public void addAccount() {
+    public void addAccount(Account account) {
         System.out.println(getClass() + " : DOING MY DB WORK : ADDING AN ACCOUNT");
     }
 }
