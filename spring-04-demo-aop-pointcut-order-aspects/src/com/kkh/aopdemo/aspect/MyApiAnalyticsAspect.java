c0266bb035ddd67cde2504af218c4ce343f8f7c7
kihwankim <clack2933@gmail.com>
2019-09-21 00:24:57 +0900

    add order and refactoring aspect code

--- spring-04-demo-aop-pointcut-order-aspects/src/com/kkh/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ spring-04-demo-aop-pointcut-order-aspects/src/com/kkh/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
+@Order(3)
+public class MyApiAnalyticsAspect {
 
     @Before("com.kkh.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n============>>> Executing @Before advice on method");
+    public void performApiAnalytics() {
+        System.out.println("\n============>>> Performing API analytics");
     }
 }
