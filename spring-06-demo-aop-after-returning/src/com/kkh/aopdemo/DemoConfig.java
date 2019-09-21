f4c7ea3d924885eca57a3427bef15c1ad613f81c
kihwankim <clack2933@gmail.com>
2019-09-21 10:41:51 +0900

    init

---
+++ spring-06-demo-aop-after-returning/src/com/kkh/aopdemo/DemoConfig.java
@@ -0,0 +1,11 @@
+package com.kkh.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.kkh.aopdemo")
+public class DemoConfig {
+}
---
+++ spring-06-demo-aop-after-returning/src/com/kkh/aopdemo/MainDemoApp.java
@@ -0,0 +1,42 @@
+package com.kkh.aopdemo;
+
+import com.kkh.aopdemo.dao.AccountDAO;
+import com.kkh.aopdemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from spring container
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // get membership bean from spring container
+        MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+        // call the business method
+        Account account = new Account();
+        account.setName("Madhu");
+        account.setLevel("diamond");
+
+        accountDAO.addAccount(account, true);
+        accountDAO.doWork();
+
+        // call the accountDAO getter/setter methods
+        accountDAO.setName("foobar");
+        accountDAO.setServiceCode("silver");
+
+        String name = accountDAO.getName();
+        String code = accountDAO.getServiceCode();
+        System.out.println("name : " + name + ", " + "code : " + code);
+
+        // call the membership business method
+        membershipDAO.addSillyMember();
+        membershipDAO.goToSleep();
+
+        // close the context
+        context.close();
+    }
+}
