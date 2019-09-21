7c2b475e8501e5911c2ffba285bd153ac37c4a2b
kihwankim <clack2933@gmail.com>
2019-09-21 11:17:50 +0900

    add AfterReturning annotation and modify the return data

--- spring-06-demo-aop-after-returning/src/com/kkh/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-06-demo-aop-after-returning/src/com/kkh/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,53 @@ package com.kkh.aopdemo.aspect;
 
 import com.kkh.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    // add a new advice for @AfterReturning on the findAccounts method
+    @AfterReturning(
+            pointcut = "execution(* com.kkh.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(
+            JoinPoint joinPoint, List<Account> result // returning 이랑 동일한 이름이어야 한다
+    ) {
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n=======>>> Executing @AfterReturning on method : " + method);
+
+        // print out the results of the method call
+        System.out.println("\n=======>>> result is : " + result);
+
+        // let's post-process the data ... let's modify it!
+
+        // convert the account names to uppercase
+        this.convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n=======>>> result is : " + result);
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+        // loop through accounts
+        for (Account account : result) {
+            // get uppercase version of name
+            String upperName = account.getName().toUpperCase();
+
+            // update the name on the account
+            account.setName(upperName);
+        }
+    }
 
     @Before("com.kkh.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint joinPoint) {
