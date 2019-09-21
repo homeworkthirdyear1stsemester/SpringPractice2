cb1f3440d6e9da5a31bc11bb3b7b5d65352e96a3
kihwankim <clack2933@gmail.com>
2019-09-21 11:56:04 +0900

    add After annotation

--- spring-06-demo-aop-after-returning/src/com/kkh/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-06-demo-aop-after-returning/src/com/kkh/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,7 @@ package com.kkh.aopdemo.aspect;
 
 import com.kkh.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -16,6 +13,13 @@ import java.util.List;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    @After("execution(* com.kkh.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(JoinPoint joinPoint) {
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n=======>>> Executing @After (finally) on method : " + method);
+    }
 
     @AfterThrowing(
             pointcut = "execution(* com.kkh.aopdemo.dao.AccountDAO.findAccounts(..))",
