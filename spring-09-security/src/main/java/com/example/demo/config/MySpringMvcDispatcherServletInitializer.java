c7e18691c043d2fe433961b4962006dcfdc8bb53
kihwankim <clack2933@gmail.com>
2020-05-07 00:44:48 +0900

    spring mvc web security demo init

---
+++ spring-09-security/src/main/java/com/example/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    // servlet mapping
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{DemoAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ spring-09-security/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>kkh company home page</title>
+</head>
+<body>
+<h2>kkh company home page</h2>
+<hr>
+Welcome to the kkh company home page!
+</body>
+</html>
