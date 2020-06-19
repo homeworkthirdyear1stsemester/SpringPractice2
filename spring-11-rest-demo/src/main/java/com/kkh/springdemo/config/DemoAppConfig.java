3399d6cedf8f7ce589d7f121f4e4506db22642e2
kihwankim <clack2933@gmail.com>
2020-06-19 20:46:19 +0900

    init rest spring project

---
+++ spring-11-rest-demo/src/main/java/com/kkh/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.kkh.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.kkh.springdemo")
+public class DemoAppConfig {
+
+}
