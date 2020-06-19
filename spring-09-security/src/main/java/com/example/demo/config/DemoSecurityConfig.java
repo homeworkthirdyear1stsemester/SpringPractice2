b51956285f94e85bd347ebbdb7a942f77c8dc479
kihwankim <clack2933@gmail.com>
2020-06-19 12:46:22 +0900

    fix bug in user name and password and connect jdbc and page

--- spring-09-security/src/main/java/com/example/demo/config/DemoSecurityConfig.java
+++ spring-09-security/src/main/java/com/example/demo/config/DemoSecurityConfig.java
@@ -1,26 +1,27 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // add a reference to our security data source
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // add our user for in memory authentication
-
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("abc").password("abc").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("bcd").password("test1").roles("MANAGER", "EMPLOYEE"))
-                .withUser(userBuilder.username("efg").password("test1").roles("ADMIN", "EMPLOYEE"));
+        // use jdbc authentication
+        auth.jdbcAuthentication().dataSource(this.securityDataSource);
+        // give spring security to use jdbc authentication with our data source
     }
 
     @Override
